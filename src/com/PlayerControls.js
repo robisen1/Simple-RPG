83e5cd0d8c2e5936e49f16e0821d91daea313a94
robi sen <robisen@gmail.com>
2014-10-01 12:24:55 -0700

    minor edits
    minor edits to camera and controls

--- src/com/PlayerControls.js
+++ src/com/PlayerControls.js
@@ -4,8 +4,11 @@ Crafty.c("PlayerControls", {
 			   //lets now make the hero move where ever we touch      
 		    Crafty.addEvent(this, Crafty.stage.elem, 'mousedown', function(e) {
 		    // lets simulate a 8 way controller or old school joystick
-
-		    console.log("the values are; x= " + e.clientX );
+		    console.log("Before we move here are the values");
+		    console.log("e.clientX is = " + e.clientX );
+		    console.log("e.clientY is = " + e.clientY );
+		    console.log("playerX is = " + player.x);
+		    console.log("playerY is = " + player.y);
 		    if (e.clientX < player.x && (e.clientX - player.x)< 32) 
 		       {player.x =  player.x - 32;}
 		   	else if (e.clientX > player.x && (e.clientX - player.x) > 32){
@@ -22,7 +25,12 @@ Crafty.c("PlayerControls", {
 		   	// let the game know we moved and where too	
 			Crafty.trigger('Moved', {x: player.x, y: player.y});
 			Crafty.trigger('Turn');
-			  	console.log('mousedown at (' + e.clientX + ', ' + e.clientY + ')');
+			console.log("After our move");
+		    console.log("e.clientX is = " + e.clientX );
+		    console.log("e.clientY is = " + e.clientY );
+		    console.log("playerX is = " + player.x);
+		    console.log("playerY is = " + player.y);
+			console.log('mousedown at (' + e.clientX + ', ' + e.clientY + ')');
 			});
 			}
 		
