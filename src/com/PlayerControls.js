9b4d065ffa8d9834f379aa6a578b57deeb946c83
robi sen <robisen@gmail.com>
2014-10-02 14:10:55 -0700

    bug fix

--- src/com/PlayerControls.js
+++ src/com/PlayerControls.js
@@ -12,35 +12,35 @@ Crafty.c("PlayerControls", {
 		    console.log("playerX is = " + player.x);
 		    console.log("playerY is = " + player.y);
 		    
-		    if (e.clientX < player.x && (e.clientX - player.x)< 32) 
+		    if (e.clientX < (player.x+Crafty.viewport.x) && (e.clientX - (player.x+Crafty.viewport.x))< 32) 
 		       {
 		       	//player.x =  player.x - 32;
 		       	console.log(" the x value is -1 ")
 		       	myx = -1;
 		       }
-		   	else if (e.clientX > player.x && (e.clientX - player.x) > 32){
+		   	else if (e.clientX > (player.x+Crafty.viewport.x) && (e.clientX - (player.x+Crafty.viewport.x)) > 32){
 		   		//player.x = player.x + 32;
 		   		console.log(" the x value is 1 ")
 		   		myx = 1;
 		   	}
-		   	else {player.x = player.x; 
+		   	else {player.x = (player.x+Crafty.viewport.x); 
 		   		console.log(" the x value is 0 ")
 		   		  myx = 0
 		   		}
 
 
-		   	if (e.clientY < player.y && (e.clientY - player.y)< 32) 
+		   	if (e.clientY < (player.y+Crafty.viewport.y) && (e.clientY - (player.y+Crafty.viewport.y))< 32) 
 		       {
 		       	//player.y =  player.y - 32;
 		       	console.log(" the y value is -1 ")
 		       	myy= -1
 		       }
-		   	else if (e.clientY > player.y && (e.clientY - player.y) > 32){
+		   	else if (e.clientY > (player.y+Crafty.viewport.y) && (e.clientY - (player.y+Crafty.viewport.y)) > 32){
 		   		//player.y = player.y + 32;
 		   		console.log(" the y value is 1 ")
 		   		myy= 1
 		   	}
-		   	else {player.y = player.y;
+		   	else {player.y = (player.y+Crafty.viewport.y);
 		   		console.log(" the y value is 0 ")
 		   		myy = 0;}
 		   	// let the game know we moved and where too	
