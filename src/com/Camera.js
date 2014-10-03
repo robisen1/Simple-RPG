a02c6a92614aa4c7209c814b211e06fdc20c4432
robi sen <robisen@gmail.com>
2014-10-02 19:48:51 -0700

    bug fixes
    fixed a bug with how direction is determined when using the mouse and
    support a 8 way movement.

--- src/com/Camera.js
+++ src/com/Camera.js
@@ -9,14 +9,10 @@ Crafty.c("Camera",{
     obj.bind("Moved",function(location) { that.set(location); });
   },
   set: function(obj) {
-    console.log("before we move the position is X " + player.x + "and Y is " + player.y) ;
-    console.log("")
+
     Crafty.viewport.x = -obj.x + Crafty.viewport.width / 2;
     Crafty.viewport.y = -obj.y + Crafty.viewport.height / 2;
-    console.log("viewport width is " + Crafty.viewport.width + " the object x is obj.x " + obj.x + " the y is obj.y"
-+ obj.y
-      ) ;
-    console.log("the players position value is now " + player.x + "and Y is " + player.y) ;
+      
 
   }
 });
--- src/com/PlayerControls.js
+++ src/com/PlayerControls.js
@@ -4,43 +4,32 @@ Crafty.c("PlayerControls", {
 			   //lets now make the hero move where ever we touch      
 		    Crafty.addEvent(this, Crafty.stage.elem, 'mousedown', function(e) {
 		    // lets simulate a 8 way controller or old school joystick
-		    console.log("Before we move here are the values");
-		    console.log("Old client X =  " + lastclientX );
-		    console.log("Old client Y =  " + lastclientY );
-		    console.log("e.clientX is = " + e.clientX );
-		    console.log("e.clientY is = " + e.clientY );
-		    console.log("playerX is = " + player.x);
-		    console.log("playerY is = " + player.y);
+		    //build out the direction of the mouse point. remember that y increases as it goes 'downward'
 		    
 		    if (e.clientX < (player.x+Crafty.viewport.x) && (e.clientX - (player.x+Crafty.viewport.x))< 32) 
 		       {
-		       	//player.x =  player.x - 32;
-		       	console.log(" the x value is -1 ")
+		       	console.log(" the x value is -1 ");
 		       	myx = -1;
 		       }
 		   	else if (e.clientX > (player.x+Crafty.viewport.x) && (e.clientX - (player.x+Crafty.viewport.x)) > 32){
-		   		//player.x = player.x + 32;
-		   		console.log(" the x value is 1 ")
+		   		console.log(" the x value is 1 ");
 		   		myx = 1;
 		   	}
-		   	else {player.x = (player.x+Crafty.viewport.x); 
-		   		console.log(" the x value is 0 ")
-		   		  myx = 0
+		   	else {console.log(" the x value is 0 ");
+		   		myx = 0;
 		   		}
 
 
 		   	if (e.clientY < (player.y+Crafty.viewport.y) && (e.clientY - (player.y+Crafty.viewport.y))< 32) 
-		       {
-		       	//player.y =  player.y - 32;
-		       	console.log(" the y value is -1 ")
-		       	myy= -1
+		       {console.log(" the y value is -1 ");
+		       	myy= -1;
 		       }
 		   	else if (e.clientY > (player.y+Crafty.viewport.y) && (e.clientY - (player.y+Crafty.viewport.y)) > 32){
-		   		//player.y = player.y + 32;
-		   		console.log(" the y value is 1 ")
-		   		myy= 1
+		   		console.log(" the y value is 1 ");
+		   		myy= 1;
 		   	}
-		   	else {player.y = (player.y+Crafty.viewport.y);
+		   	else {
+		   		//player.y = (player.y+Crafty.viewport.y);
 		   		console.log(" the y value is 0 ")
 		   		myy = 0;}
 		   	// let the game know we moved and where too	
@@ -49,12 +38,7 @@ Crafty.c("PlayerControls", {
 		   	this.trigger('Slide',direction);
 			//Crafty.trigger('Moved', {x: player.x, y: player.y});
 			Crafty.trigger('Turn');
-			console.log("the direction is = " + direction)
-			console.log("After our move");
-		    console.log("e.clientX is = " + e.clientX );
-		    console.log("e.clientY is = " + e.clientY );
-		    console.log("playerX is = " + player.x);
-		    console.log("playerY is = " + player.y);
+			
 
 		    lastclientY = e.clientY;
 		    lastclientX = e.clientX;
